bc10332d7b5521a994a994545c798c2148eca0cc
Charles Romney <romney.charles@gmail.com>
2017-05-20 19:50:37 +0200

    extend utils with ValueHelper

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,14 +1,16 @@
 import { DEBUG } from "../../constant";
 import BodyHelper from "./BodyHelper";
 import StringHelper from "./StringHelper";
+import ValueHelper from "./ValueHelper";
 
 // ------------------- //
   // CLASS UTILS  ------------------- //
 // ------------------- //
 
 const Utils = {
-  body: BodyHelper,
-  string: StringHelper,
+  body    :   BodyHelper,
+  string  :   StringHelper,
+  value   :   ValueHelper
 };
 
 // ------------------- //
